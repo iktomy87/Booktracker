30f6d107603450a851182a3d9e90bc3e08dcd106
iktomy87 <tomyrodriguez1939@gmail.com>
2026-02-27 22:50:22 -0300

    feat: back configurado para guardar imagenes

--- demo/src/main/java/com/booktracker/demo/config/WebConfig.java
+++ demo/src/main/java/com/booktracker/demo/config/WebConfig.java
@@ -3,10 +3,12 @@ package com.booktracker.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
     
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -20,5 +22,12 @@ public class WebConfig {
                     .allowCredentials(true);
             }
         };
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Le dice a Spring que cualquier petición a /uploads/** busque en la carpeta local "uploads/"
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:uploads/");
     }
 }
